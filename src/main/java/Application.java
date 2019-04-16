5bdc4a369774497a1caa5adc9ae3b94c4523674e
Andre Rocha <andrefmrocha@live.com.pt>
2019-04-16 18:40:13 +0100

    Added very basic menu

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,13 @@
 import com.lpoo_32.view.Display;
 import com.lpoo_32.view.Game;
+import com.lpoo_32.view.Menu;
 
 import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) {
         try{
-            Display menu = new Game();
+            Display menu = new Menu();
             menu.draw();
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/lpoo_32/view/Menu.java
+++ src/main/java/com/lpoo_32/view/Menu.java
@@ -1,25 +1,35 @@
 package com.lpoo_32.view;
 
 import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.gui2.ActionListBox;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.*;
 
 import java.io.IOException;
 
 public class Menu extends Display {
     ActionListBox listBox;
+    BasicWindow window;
+    MultiWindowTextGUI gui;
+    Panel panel;
+
     public Menu() throws IOException {
         super();
         TerminalSize size = new TerminalSize(10, 3);
         this.listBox = new ActionListBox(size);
+        this.panel = new Panel();
+        this.window = new BasicWindow();
+        this.gui = new MultiWindowTextGUI(this.screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.MAGENTA));
         this.addOptions();
+        this.window.setComponent(panel);
+        this.gui.addWindowAndWait(window);
     }
 
     @Override
-    public void draw() {
-//        listBox.draw(this.gui.);
+    public void draw() throws IOException {
+        this.gui.updateScreen();
     }
 
     private void addOptions(){
-        this.listBox.addItem("Meias", () -> System.out.println("MEIAAAAS"));
+        this.listBox.addItem("Meias", () -> System.out.println("MEIAAAAS")).addTo(this.panel);
     }
 }
