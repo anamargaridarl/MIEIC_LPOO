88a7175a71b7af8284626ca8c5bd33ab97985fe1
Andre Rocha <andrefmrocha@live.com.pt>
2019-04-18 19:19:51 +0100

    Added a secondary menu key Listener

--- src/main/java/com/lpoo_32/Controller/SecondaryMenuKeyListener.java
+++ src/main/java/com/lpoo_32/Controller/SecondaryMenuKeyListener.java
@@ -2,6 +2,7 @@ package com.lpoo_32.Controller;
 
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
 import com.googlecode.lanterna.gui2.Window;
 import com.googlecode.lanterna.gui2.WindowListener;
 import com.googlecode.lanterna.input.KeyStroke;
@@ -9,7 +10,15 @@ import com.googlecode.lanterna.input.KeyType;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class KeyStrokeListener implements WindowListener {
+public class SecondaryMenuKeyListener implements WindowListener {
+
+    private final MultiWindowTextGUI gui;
+    private final Window window;
+
+    public SecondaryMenuKeyListener(MultiWindowTextGUI gui, Window window){
+        this.gui = gui;
+        this.window = window;
+    }
     @Override
     public void onResized(Window window, TerminalSize oldSize, TerminalSize newSize) {
 
@@ -24,7 +33,7 @@ public class KeyStrokeListener implements WindowListener {
     public void onInput(Window basePane, KeyStroke keyStroke, AtomicBoolean deliverEvent) {
         if(keyStroke.getKeyType().equals(KeyType.Character)){
             if(keyStroke.getCharacter() == 'q')
-                System.exit(0);
+                this.gui.removeWindow(this.window);
         }
     }
 
--- src/main/java/com/lpoo_32/view/Menu.java
+++ src/main/java/com/lpoo_32/view/Menu.java
@@ -3,7 +3,8 @@ package com.lpoo_32.view;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.gui2.*;
-import com.lpoo_32.Controller.KeyStrokeListener;
+import com.lpoo_32.Controller.SecondaryMenuKeyListener;
+import com.lpoo_32.Controller.MenuKeyListener;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -11,24 +12,41 @@ import java.util.Arrays;
 public class Menu extends Display {
     private ActionListBox listBox;
     private MultiWindowTextGUI gui;
-    private Panel panel;
+    private Panel mainPanel;
 
     public Menu() throws IOException {
         super();
         Panel mainPanel = createMainMenu();
-        BasicWindow window = new BasicWindow();
-        this.gui = new MultiWindowTextGUI(this.screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.MAGENTA));
         this.addOptions();
+        this.gui = new MultiWindowTextGUI(this.screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.MAGENTA));
+        Window mainWindow = createWindow(mainPanel);
+        mainWindow.addWindowListener(new MenuKeyListener());
+        this.gui.addWindowAndWait(mainWindow);
+    }
+
+    private Window createWindow(Panel mainPanel) {
+        BasicWindow window =  new BasicWindow();
         window.setComponent(mainPanel);
         window.setHints(Arrays.asList(Window.Hint.CENTERED, Window.Hint.FIT_TERMINAL_WINDOW));
-        window.addWindowListener(new KeyStrokeListener());
-        this.gui.addWindowAndWait(window);
+        return window;
     }
 
     private Panel createMainMenu() {
         TerminalSize size = new TerminalSize(20, 5);
         this.listBox = new ActionListBox(size);
-        this.panel = new Panel();
+        this.mainPanel = new Panel();
+        return getPanel(mainPanel);
+    }
+
+    private Panel createHintMenu(){
+        TerminalSize size = new TerminalSize(20, 5);
+        TextBox textBox = new TextBox(size, "You move around with your arrow keys");
+        Panel hintPanel = new Panel();
+        hintPanel.addComponent(textBox);
+        return getPanel(hintPanel);
+    }
+
+    private Panel getPanel(Panel panel) {
         panel.setLayoutManager(new BorderLayout());
         panel.setLayoutData(BorderLayout.Location.CENTER);
         Panel mainPanel = new Panel();
@@ -50,7 +68,7 @@ public class Menu extends Display {
     }
 
     private void addOptions(){
-        this.listBox.addItem("Meias", () -> System.out.println("MEIAAAAS")).addTo(this.panel);
+        this.listBox.addItem("Meias", () -> System.out.println("MEIAAAAS")).addTo(this.mainPanel);
         this.listBox.addItem("Start", ()-> {
             try {
                 Display  game = new Game(this.screen);
@@ -58,6 +76,11 @@ public class Menu extends Display {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }).addTo(this.panel);
+        }).addTo(this.mainPanel);
+        this.listBox.addItem("Help", ()->{
+            Window window = this.createWindow(this.createHintMenu());
+            window.addWindowListener(new SecondaryMenuKeyListener(this.gui, window));
+            this.gui.addWindow(window);
+        });
     }
 }
