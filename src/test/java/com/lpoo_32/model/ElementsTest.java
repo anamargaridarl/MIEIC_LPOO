f6de5cf787481a9037648a381c3916aa734a040d
Andre Rocha <andrefmrocha@live.com.pt>
2019-04-19 12:51:10 +0100

    Added further tests into elements

--- src/test/java/com/lpoo_32/model/ElementsTest.java
+++ src/test/java/com/lpoo_32/model/ElementsTest.java
@@ -2,8 +2,7 @@ package com.lpoo_32.model;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 public class ElementsTest {
 
@@ -15,5 +14,12 @@ public class ElementsTest {
         elements.addElement(new SpikesModel(10,new Position(5,5)));
 
         assertNotEquals(null,elements.getValue(new Position(4,5)));
+
+        InteractableElement element = new FoodModel(20, new Position(10, 5));
+        elements.addElement(element);
+
+        assertEquals(element, elements.getValue(element.getPos()));
+
+        assertNull(elements.getValue(new Position(5, 10)));
     }
 }
