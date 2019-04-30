af2bd4a749416954315084eb726b6071239c7056
Andre Rocha <andrefmrocha@live.com.pt>
2019-04-30 12:13:02 +0100

    Refactored controller

---
+++ src/main/java/com/lpoo_32/view/EventType.java
@@ -0,0 +1,16 @@
+package com.lpoo_32.view;
+
+public enum EventType{
+    MOVEUP,
+    MOVEDOWN,
+    MOVELEFT,
+    MOVERIGHT,
+    EXIT,
+    HELP,
+    STORE,
+    USE,
+    LEFTINVENTORY,
+    RIGHTINVENTORY,
+    INVETORYUSE,
+    NULL
+}
