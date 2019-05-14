ace714a596b2b0ad6bfc721107c5d9fb4e790739
Andre Rocha <andrefmrocha@live.com.pt>
2019-05-14 11:03:04 +0100

    Added wall view

---
+++ code/src/main/java/com/lpoo_32/view/WallView.java
@@ -0,0 +1,16 @@
+package com.lpoo_32.view;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.lpoo_32.model.WallModel;
+
+public class WallView extends InteractableElementView{
+
+    public WallView(WallModel element) {
+        super(element);
+    }
+
+    @Override
+    public void draw(TextGraphics graphics) {
+
+    }
+}
