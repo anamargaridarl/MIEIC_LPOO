17a97a9979e095c770fd815e28f720254d6788b0
Andre Rocha <andrefmrocha@live.com.pt>
2019-05-21 11:10:34 +0100

    Added testing

--- code/src/test/java/com/lpoo_32/model/NourishStateTest.java
+++ code/src/test/java/com/lpoo_32/model/NourishStateTest.java
@@ -1,19 +1,23 @@
 package com.lpoo_32.model;
 
-import com.lpoo_32.exceptions.HealthOVF;
-import com.lpoo_32.exceptions.HungerOVF;
-import com.lpoo_32.exceptions.ThirstOVF;
+import com.lpoo_32.exceptions.*;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 public class NourishStateTest {
     private PlayerModel player;
     private Status status;
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
     @Before
     public void initializeValues(){
         player = Mockito.mock(PlayerModel.class);
@@ -24,23 +28,39 @@ public class NourishStateTest {
     }
 
     @Test
-    public void sated() throws HungerOVF, HealthOVF, ThirstOVF {
+    public void sated() throws HungerOVF, HealthOVF, ThirstOVF, HungerRestored, ThirstRestored, Sleeptime {
         NourishState sated = new SatedState(player);
         sated.update(5400);
         verify(status).decreaseValue(5);
     }
 
     @Test
-    public void quenched() throws HungerOVF, HealthOVF, ThirstOVF {
+    public void quenched() throws HungerOVF, HealthOVF, ThirstOVF, HungerRestored, ThirstRestored, Sleeptime {
         NourishState quenched = new QuenchedState(player);
         quenched.update(3600);
         verify(status).decreaseValue(5);
     }
 
     @Test
-    public void famish() throws HungerOVF, HealthOVF, ThirstOVF {
+    public void famish() throws HungerOVF, HealthOVF, ThirstOVF, HungerRestored, ThirstRestored, Sleeptime {
         NourishState famish = new FamishState(player);
         famish.update(120);
         verify(status).decreaseValue(5);
+    }
+
+    @Test
+    public void sleep() throws HungerOVF, ThirstOVF, HealthOVF, ThirstRestored, Sleeptime, HungerRestored {
+        NourishState sleep =  new SleepState(player);
+        thrown.expect(Sleeptime.class);
+        sleep.update(0);
+    }
+
+    @Test
+
+    public void day() throws HungerOVF, ThirstOVF, HealthOVF, ThirstRestored, Sleeptime, HungerRestored {
+        NourishState day = new DayState(player);
+        day.update(0);
+        verify(status, never()).decreaseValue(any(int.class));
+        verify(status, never()).increaseValue(any(int.class));
     }
 }
