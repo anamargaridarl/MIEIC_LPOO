2552a87cc36e62c03f7994ae3509fe46e9c3deda
Andre Rocha <andrefmrocha@live.com.pt>
2019-05-27 22:36:28 +0100

    Progress on menu

--- code/src/main/java/com/lpoo_32/view/MenuSwingGUI.java
+++ code/src/main/java/com/lpoo_32/view/MenuSwingGUI.java
@@ -25,69 +25,6 @@ public class MenuSwingGUI extends JFrame {
     private JPanel root;
     private String[] choices = { "Game", "Instructions", "Exit" };
     JList<String> list = new JList(choices);
-    private SharedListSelectionHandler sharedListSelectionHandler;
-
-
-    class SharedListSelectionHandler implements ListSelectionListener {
-        public void valueChanged(ListSelectionEvent listSelectionEvent) {
-            System.out.print("First index: " + listSelectionEvent.getFirstIndex());
-            System.out.print(", Last index: " + listSelectionEvent.getLastIndex());
-            boolean adjust = listSelectionEvent.getValueIsAdjusting();
-            System.out.println(", Adjusting? " + adjust);
-            if (!adjust) {
-                JList list = (JList) listSelectionEvent.getSource();
-
-                int selections[] = list.getSelectedIndices();
-                Object selectionValues[] = list.getSelectedValues();
-                for (int i = 0, n = selections.length; i < n; i++) {
-                    if (i == 0) {
-                        System.out.print("  Selections: ");
-                    }
-                    System.out.print(selections[i] + "/" + selectionValues[i] + " ");
-                    if(selections[i] == 0)
-                    {
-                        Elements elements = new Elements();
-                        PlayerModel model = null;
-                        try {
-                            model = new PlayerModel(new Position(2,2, Game.width/4, Game.height/4, 0));
-                        } catch (OutOfBoundaries outOfBoundaries) {
-                            outOfBoundaries.printStackTrace();
-                        }
-                        JFrame frame = new JFrame();
-                        GameController game = null;
-                        try {
-                            game = new GameController(
-                                    elements,
-                                    model,
-                                    new GameSwing(frame, model, elements)
-                                    );
-                        } catch (OutOfBoundaries outOfBoundaries) {
-                            outOfBoundaries.printStackTrace();
-                        }
-                        frame.addKeyListener(new SwingKeyboard(game));
-                        try {
-                            dispose();
-                            game.run();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-                        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-                        System.out.println("Stopping game");
-
-                    }
-                    else if(selections[i] == 1)
-                    {
-                        dispose();
-                        HelpMenuSwing help = new HelpMenuSwing();
-                        help.run();
-                    }
-
-                }
-                System.out.println();
-            }
-        }
-    };
 
 
     MouseListener mouseListener = new MouseAdapter() {
@@ -110,7 +47,74 @@ public class MenuSwingGUI extends JFrame {
         list.setFixedCellHeight(100);
         list.setFixedCellWidth(50);
         this.getContentPane().add(list);
-        list.addListSelectionListener(sharedListSelectionHandler);
+        list.addListSelectionListener((listSelectionEvent) -> {
+                System.out.print("First index: " + listSelectionEvent.getFirstIndex());
+                System.out.print(", Last index: " + listSelectionEvent.getLastIndex());
+                boolean adjust = listSelectionEvent.getValueIsAdjusting();
+                System.out.println(", Adjusting? " + adjust);
+                if (!adjust) {
+                    JList list = (JList) listSelectionEvent.getSource();
+
+                    int[] selections = list.getSelectedIndices();
+                    Object[] selectionValues = list.getSelectedValues();
+                    for (int i = 0, n = selections.length; i < n; i++) {
+                        if (i == 0) {
+                            System.out.print("  Selections: ");
+                        }
+                        System.out.print(selections[i] + "/" + selectionValues[i] + " ");
+                        if(selections[i] == 0)
+                        {
+                            Elements elements = new Elements();
+                            PlayerModel model = null;
+                            try {
+                                model = new PlayerModel(new Position(2,2, Game.width/4, Game.height/4, 0));
+                            } catch (OutOfBoundaries outOfBoundaries) {
+                                outOfBoundaries.printStackTrace();
+                            }
+                            JFrame frame = new JFrame();
+                            GameController game = null;
+                            try {
+                                game = new GameController(
+                                        elements,
+                                        model,
+                                        new GameSwing(frame, model, elements)
+                                );
+                            } catch (OutOfBoundaries outOfBoundaries) {
+                                outOfBoundaries.printStackTrace();
+                            }
+                            this.list.removeMouseListener(mouseListener);
+                            frame.addKeyListener(new SwingKeyboard(game));
+                            this.addKeyListener(new SwingKeyboard(game));
+                            root.addKeyListener(new SwingKeyboard(game));
+                            this.setVisible(false);
+                            try {
+                                game.run();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                            frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+                            this.setVisible(true);
+                            this.pack();
+                            this.manageList();
+                        System.out.println("Stopping game");
+                        }
+                        else if(selections[i] == 1)
+                        {
+                            this.setVisible(false);
+                            HelpMenuGUI help = new HelpMenuGUI(this);
+                            help.setVisible(true);
+                        }
+                        else if (selections[i] == 2){
+                            this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                            this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+
+                        }
+
+                    }
+                    System.out.println();
+                }
+        });
         list.addMouseListener(mouseListener);
     }
 
@@ -121,7 +125,6 @@ public class MenuSwingGUI extends JFrame {
         add(root);
         setTitle("Menu");
         setSize(400,330);
-        sharedListSelectionHandler = new SharedListSelectionHandler();
         manageList();
     }
 
