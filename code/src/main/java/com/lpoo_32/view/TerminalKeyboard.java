8638c32939e4c755db03d2632c698d10272acc18
Andre Rocha <andrefmrocha@live.com.pt>
2019-06-01 15:49:11 +0100

    Added tests for terminal keyboard

--- code/src/main/java/com/lpoo_32/view/TerminalKeyboard.java
+++ code/src/main/java/com/lpoo_32/view/TerminalKeyboard.java
@@ -30,71 +30,76 @@ public class TerminalKeyboard implements KeyboardAnalyzer {
 
     public void processKey() throws IOException {
         while (flag){
-            ActionEvent event = null;
-            KeyStroke key = screen.readInput();
-            if (key != null) {
-                switch (key.getKeyType()) {
-                    case ArrowUp:
-                        event = new MoveUp(controller);
-                        break;
-                    case ArrowDown:
-                        event = new MoveDown(controller);
-                        break;
-                    case ArrowLeft:
-                        event = new MoveLeft(controller);
-                        break;
-                    case ArrowRight:
-                        event = new MoveRight(controller);
-                        break;
-                    case Character:
-                        switch (key.getCharacter()) {
-                            case 'z':
-                                event = new Exit();
-                                break;
-                            case 'h':
-                                event = new Null();
-                                //TODO: How to proceed with this?
-                                System.out.println("Open help Menu");
-                                break;
-                            case 'f': //add element to inventory
-                                event = new Store(controller);
-                                break;
-                            case 't': //use water/food in moment
-                                event = new Use(controller);
-                                break;
-                            case '1': //move left in inventory
-                                event = new LeftInventory(controller.getPlayer());
-                                break;
-                            case '2': //move right in inventory
-                                event = new RightInventory(controller.getPlayer());
-                                break;
-                            case 'e':
-                                event = new InventoryUse(controller);
-                                break;
-                            case 'w':
-                                event = new AttackUp(controller);
-                                break;
-                            case 'd':
-                                event = new AttackRight(controller);
-                                break;
-                            case 'a':
-                                event = new AttackLeft(controller);
-                                break;
-                            case 's':
-                                event = new AttackDown(controller);
-                                break;
-                            default:
-                                event = new Null();
-                                break;
-                        }
-                        break;
-                    default:
-                        event = new Null();
-                        break;
-                }
-            }
+            ActionEvent event = getActionEvent();
             this.controller.processKey(event);
         }
+    }
+
+    ActionEvent getActionEvent() throws IOException {
+        ActionEvent event = null;
+        KeyStroke key = screen.readInput();
+        if (key != null) {
+            switch (key.getKeyType()) {
+                case ArrowUp:
+                    event = new MoveUp(controller);
+                    break;
+                case ArrowDown:
+                    event = new MoveDown(controller);
+                    break;
+                case ArrowLeft:
+                    event = new MoveLeft(controller);
+                    break;
+                case ArrowRight:
+                    event = new MoveRight(controller);
+                    break;
+                case Character:
+                    switch (key.getCharacter()) {
+                        case 'z':
+                            event = new Exit();
+                            break;
+                        case 'h':
+                            event = new Null();
+                            //TODO: How to proceed with this?
+                            System.out.println("Open help Menu");
+                            break;
+                        case 'f': //add element to inventory
+                            event = new Store(controller);
+                            break;
+                        case 't': //use water/food in moment
+                            event = new Use(controller);
+                            break;
+                        case '1': //move left in inventory
+                            event = new LeftInventory(controller.getPlayer());
+                            break;
+                        case '2': //move right in inventory
+                            event = new RightInventory(controller.getPlayer());
+                            break;
+                        case 'e':
+                            event = new InventoryUse(controller);
+                            break;
+                        case 'w':
+                            event = new AttackUp(controller);
+                            break;
+                        case 'd':
+                            event = new AttackRight(controller);
+                            break;
+                        case 'a':
+                            event = new AttackLeft(controller);
+                            break;
+                        case 's':
+                            event = new AttackDown(controller);
+                            break;
+                        default:
+                            event = new Null();
+                            break;
+                    }
+                    break;
+                default:
+                    event = new Null();
+                    break;
+            }
+        }
+        return event;
     }
 
     void stopKeyboard(){
--- code/src/test/java/com/lpoo_32/view/TerminalKeyboardTest.java
+++ code/src/test/java/com/lpoo_32/view/TerminalKeyboardTest.java
@@ -1,39 +1,66 @@
 package com.lpoo_32.view;
 
-//TODO: FIX IT
-/*public class TerminalKeyboardTest {
-    @Test
-    public void processKey() throws IOException, ThirstOVF, HungerOVF {
-        Screen screen = Mockito.mock(Screen.class);
-        TerminalKeyboard keyboard = new TerminalKeyboard(screen);
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke(KeyType.ArrowRight));
-        Assert.assertEquals(EventType.MOVERIGHT, keyboard.processKey());
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke(KeyType.ArrowUp));
-        assertEquals(EventType.MOVEUP, keyboard.processKey());
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke(KeyType.ArrowDown));
-        assertEquals(EventType.MOVEDOWN, keyboard.processKey());
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke(KeyType.ArrowLeft));
-        assertEquals(EventType.MOVELEFT, keyboard.processKey());
 
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke('z', false, false));
-        assertEquals(EventType.EXIT, keyboard.processKey());
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke('h', false, false));
-        assertEquals(EventType.HELP, keyboard.processKey());
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke('f', false, false));
-        assertEquals(EventType.STORE, keyboard.processKey());
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke('2', false, false));
-        assertEquals(EventType.LEFTINVENTORY, keyboard.processKey());
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke('1', false, false));
-        assertEquals(EventType.RIGHTINVENTORY, keyboard.processKey());
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke('a', false, false));
-        assertEquals(EventType.ATTACKLEFT, keyboard.processKey());
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke('s', false, false));
-        assertEquals(EventType.ATTACKDOWN, keyboard.processKey());
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke('d', false, false));
-        assertEquals(EventType.ATTACKRIGHT, keyboard.processKey());
-        Mockito.when(screen.pollInput()).thenReturn(new KeyStroke('w', false, false));
-        assertEquals(EventType.ATTACKUP, keyboard.processKey());
-        Mockito.when(screen.pollInput()).thenReturn(null);
-        assertEquals(null, keyboard.processKey());
-    }
-}*/
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.lpoo_32.controller.GameController;
+import com.lpoo_32.controller.action.*;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class TerminalKeyboardTest {
+
+   @Test
+    public void testEvent() throws IOException {
+       Screen screen = Mockito.mock(Screen.class);
+       ActionEvent action;
+       TerminalKeyboard terminalKeyboard = new TerminalKeyboard(screen, Mockito.mock(GameController.class));
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke(KeyType.ArrowRight));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), MoveRight.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke(KeyType.ArrowUp));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), MoveUp.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke(KeyType.ArrowDown));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), MoveDown.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke(KeyType.ArrowLeft));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), MoveLeft.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke('z', false, false));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), Exit.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke('f', false, false));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), Store.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke('t', false, false));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), Use.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke('1', false, false));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), LeftInventory.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke('2', false, false));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), RightInventory.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke('e', false, false));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), InventoryUse.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke('w', false, false));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), AttackUp.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke('s', false, false));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), AttackDown.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke('a', false, false));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), AttackLeft.class);
+       Mockito.when(screen.readInput()).thenReturn(new KeyStroke('d', false, false));
+       action = terminalKeyboard.getActionEvent();
+       assertEquals(action.getClass(), AttackRight.class);
+   }
+}
